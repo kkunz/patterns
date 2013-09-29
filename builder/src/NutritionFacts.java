f47658e4ed8c9389dc28649e9cb660fb264ec89f
Karl Kunz <karlkunzjr@gmail.com>
2013-09-29 19:20:10 -0400

    adding code for builder pattern

---
+++ builder/src/NutritionFacts.java
@@ -0,0 +1,78 @@
+
+class NutritionFacts {
+
+	private final int servingSize;
+	private final int servings;
+	private final int calories;
+	private final int fat;
+	private final int sodium;
+	private final int carbohydrate;
+	
+	public static class Builder {
+		
+		//required parameters
+		
+		private final int servingSize;
+		private final int servings;
+		
+		// Optional parameters 
+		private int calories = 0;
+		private int fat = 0;
+		private int sodium = 0;
+		private int carbohydrate = 0;
+		
+		public Builder(int servingSize, int servings) {
+			
+			this.servingSize = servingSize;
+			this.servings = servings;
+			
+		}
+		
+		public Builder calories(int val) {
+			calories = val; 
+		 	return this;
+		}
+		
+		public Builder fat(int val) {
+			fat = val; 
+		 	return this;
+		}
+		
+		public Builder carbohydrate(int val) {
+			carbohydrate = val; 
+		 	return this;
+		}
+
+		public Builder sodium(int val) {
+			sodium = val; 
+		 	return this;
+		}
+		
+		public NutritionFacts build() {
+			return new NutritionFacts(this);
+			
+		}
+	}
+	
+	
+	private NutritionFacts(Builder builder) {
+		
+		servingSize = builder.servingSize;
+		servings = builder.servingSize;
+		calories = builder.servingSize;
+		fat = builder.servingSize;
+		sodium = builder.servingSize;
+		carbohydrate = builder.servingSize;
+		
+	}
+	
+	public void print() {
+		
+		System.out.println("serving size = " + servingSize);
+		System.out.println("servings = " + servings);
+		System.out.println("calories = " + calories);
+		System.out.println("fat = " + fat);
+		System.out.println("sodium = " + sodium);
+	}
+	
+}
